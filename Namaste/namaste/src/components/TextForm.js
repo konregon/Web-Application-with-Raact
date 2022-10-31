c6134e99efd3f831d4935010fd449fca8d0bb702
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-10-31 14:01:47 +0530

    adding additional function
    Word counting,
    Characters counting,
    Time to read,
    Preview,
    Lowercase,
    Speak/auto reading

--- Namaste/namaste/src/components/TextForm.js
+++ Namaste/namaste/src/components/TextForm.js
@@ -7,6 +7,21 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick = () => {
+        console.log("Lowercase was clicked" + text);
+        let newText = text.italics();
+        setText(newText);
+    }
+    const handleItClick = () => {
+        console.log("Italic was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const speak = () => {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        window.speechSynthesis.speak(msg);
+      }
 
     const handleOnChange = (event) => {
         console.log("there WAS SOMEthing!");
@@ -14,10 +29,11 @@ export default function TextForm(props) {
     }
 
      // Declare a new state variable, which we'll call "text"
-  const [text, setText] = useState('Enter Text here');
+  const [text, setText] = useState('');
 
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             {/* <div className="mb-5"> */}
             {/* <label for="myBox" className="form-label">
@@ -33,6 +49,20 @@ export default function TextForm(props) {
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
             </div>
             <div className="btn btn-primary" onClick={handleUpClick}>Change to UpperCase</div>
+            <div className="btn btn-primary mx-2" onClick={handleLoClick}>Change to LowerCase</div>
+            <div className="btn btn-primary mx-2" onClick={handleItClick}>Change to Italic</div>
+            <div onClick={speak} className="btn btn-warning mx-2">Speak</div>
+           
+
+
         </div>
+        <div className="container" my-3>
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters are written here.</p>
+            <p>It will take average person {0.008 * text.split(" ").length} min to read these text</p>
+            <h2>Preview of Text</h2>
+            <p>{text}</p>
+        </div>
+        </>
     );
 }
